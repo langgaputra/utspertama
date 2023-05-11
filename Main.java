7dd0bf72951af619d8f4c28cf5975a2626eba8b7
Langga Putra <langgabedegong@gmail.com>
2023-05-11 16:45:19 +0700

    UTS G.111.20.0044

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
+import pengguna.*;
+
 class Main {
     public static void main(String[] args) {
+        Mhs mhs = new Mhs("username","password");
+
+        mhs.setNama(" Langga Putra R.S");
+        mhs.setNim("G.111.20.0044");
+        mhs.setSmt(6);
+        
+        System.out.println("Nama    :" + mhs.getNama());
+        System.out.println("Nim    :" + mhs.getNim());
+        System.out.println("Smt    :" + mhs.getSmt());
+        System.out.println("Username   :" + mhs.getUsername());
+        System.out.println("Password  :" + mhs.getPassword());
+
 
     }
 }
--- pengguna/User.java
+++ pengguna/User.java
@@ -1,5 +1,24 @@
 package pengguna;
 
-public class User {
+public class user {
+    private String username;
+    private String password;
+
+    public void User(String arg_username, String arg_password) {
+        this.username = arg_username;
+        this.password = arg_password;
+    }
+    public void setUsername(String username) {
+        this.username =username;
+    }
+    public void setPassword(String password) {
+        this.password =password;
+    }
+    public String getUsername() {
+        return this.username = username;
+    }
+    public String getPassword() {
+        return this.password = password;
+    }
 
 }
